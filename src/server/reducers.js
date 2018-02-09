74d4563370b085be6f690074df3b97418ebfc6b2
David Moreau <dav.m85@gmail.com>
2018-02-09 14:44:54 -0500

    reducer tets

--- src/server/reducers.js
+++ src/server/reducers.js
@@ -99,8 +99,6 @@ function players (state = null, action) {
   switch (action.type) {
     case Actions.DB_READ:
       return action.data.players
-    case Actions.COMPUTE_RANK:
-
     default:
       return state
   }
@@ -167,6 +165,64 @@ function createReducer (defaultReducer, actionReducers) {
 //   }
 // }
 
+function updatePlayers (players, game) {
+  // Add missing player if not in players list
+  game.redPlayers.concat(game.bluePlayers).forEach(p => {
+    if (players.filter(fp => fp.name === p).length === 0) {
+      players.push({
+        name: p,
+        scored: 0,
+        gameCount: 0,
+        winCount: 0,
+        // trueskill default parameters
+        mu: 25.000,
+        sigma: 8.333
+      })
+    }
+  })
+
+  // look for a player and return its ratings
+  const indexOf = function (p) {
+    const index = players.findIndex(fp => fp.name === p)
+    if (index < 0) { throw new Error('cannot find player') }
+    return index
+  }
+  // // var newRank = Math.floor((player.mu - 3*player.sigma)*10)/10;
+  // // player.rankingDelta = Math.floor((player.ranking - newRank)*10)/10;
+  const toRating = function (id) {
+    return new Rating(players[id].mu, players[id].sigma)
+  }
+
+  const rankThem = function (winners, losers) {
+    let winIds = winners.map(indexOf)
+    let loseIds = losers.map(indexOf)
+    const [winnersRatings, losersRating] = rate([
+      winIds.map(toRating), loseIds.map(toRating)
+    ])
+    winIds.forEach((id, k) => {
+      players[id].mu = winnersRatings[k].mu
+      players[id].sigma = winnersRatings[k].sigma
+      players[id].scored += Math.max(game.redScore, game.blueScore)
+      players[id].gameCount += 1
+      players[id].winCount += 1
+    })
+    loseIds.forEach((id, k) => {
+      players[id].mu = losersRating[k].mu
+      players[id].sigma = losersRating[k].sigma
+      players[id].scored += Math.min(game.redScore, game.blueScore)
+      players[id].gameCount += 1
+    })
+  }
+
+  if (game.redScore > game.blueScore) {
+    rankThem(game.redPlayers, game.bluePlayers)
+  } else {
+    rankThem(game.bluePlayers, game.redPlayers)
+  }
+
+  return players.map(p => Object.assign({}, p))
+}
+
 // This special function archives a game
 module.exports = createReducer(
   combineReducers({
@@ -179,79 +235,20 @@ module.exports = createReducer(
     'ARCHIVE': function (state, action) {
       const g = state.currentGame
       if (g && (g.is === 'win' || g.is === 'cancelled')) {
-        // look for a player and return its ratings
-        const lookFor = function (p) {
-          let found = state.players.filter(fp => fp.name === p)
-          if (!found) {
-            found = {
-              name: p,
-              scored: 0,
-              gameCount: 0,
-              winCount: 0,
-              // trueskill parameters
-              mu: 25.000,
-              sigma: 8.333
-            }
-          }
-          return Object.assign({}, found)
-        }
-        // var newRank = Math.floor((player.mu - 3*player.sigma)*10)/10;
-        // player.rankingDelta = Math.floor((player.ranking - newRank)*10)/10;
-        const toRating = function (p) {
-          return new Rating(p.mu, p.sigma)
-        }
-
-        // Compute players score :)
+        // Compute player mu/sigma
         let players = state.players
         if (g.is === 'win') {
-          let winners, losers
-          if (g.redScore > g.blueScore) {
-            winners = g.redPlayers.map(p => lookFor(p))
-            losers = g.bluePlayers.map(p => lookFor(p))
-          }
-          const [winnersRatings, losersRating] = rate([winners.map(toRating), losers.map(toRating)])
-          // Replace ratings in winners/losers arrays
-          for (let i = 0; i < winners.length; i++) {
-            winners[i].mu = winnersRatings[i].mu
-            winners[i].sigma = winnersRatings[i].sigmamu
-          }
-          for (let i = 0; i < losers.length; i++) {
-            losers[i].mu = losersRating[i].mu
-            losers[i].sigma = losersRating[i].sigmamu
-          }
-          // Replace in players array
-          players = players.map(p => {
-            let w = winners.filter(w => w.name === p.name)
-            if (w.length > 0) {
-              return w.shift()
-            }
-            let los = losers.filter(w => w.name === p.name)
-            if (los.length > 0) {
-              return los.shift()
-            }
-            return Object.assign({}, p)
-          })
-          // New players ? should add them
-          winners.concat(losers).map(m => {
-            let w = players.filter(w => w.name === m.name)
-            if (w.length === 0) {
-              players.push(m)
-            }
-          })
+          players = updatePlayers(players, state.currentGame)
         }
 
-        // player.gameCount++;
-        // player.scored += status.blueScore;
-        // player.winCount +=
-
-        return Object.assign({}, state, {
+        state = Object.assign({}, state, {
           games: [...state.games, state.currentGame],
           currentGame: null,
           players
         })
       }
 
-      // store to db :)
+      // @todo store to db :)
       return state
     }
   }
---
+++ tests/reducers.test.js
@@ -0,0 +1,33 @@
+const reducers = require('../src/server/reducers')
+const Actions = require('../src/actions')
+
+const aWonGame = {
+  is: 'win',
+  redPlayers: ['dav'],
+  redScore: 5,
+  bluePlayers: ['chr'],
+  blueScore: 1
+}
+
+test('ARCHIVE moves current game to games history', () => {
+  let state = reducers({
+    currentGame: aWonGame,
+    players: [],
+    games: []
+  }, Actions.archive())
+
+  expect(state.currentGame).toBeNull()
+  expect(state.games[0]).toBeTruthy()
+})
+
+test('ARCHIVE computes new player rank', () => {
+  let state = reducers({
+    currentGame: aWonGame,
+    players: [],
+    games: []
+  }, Actions.archive())
+
+  console.log(state)
+
+  expect(state.players.filter(a => a.name === 'dav').pop().mu).toBeTruthy()
+})
